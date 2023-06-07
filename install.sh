2b45362b9ea2d6d48930df69960775d429c8bb66
jesse.lin <hsiao.jung.lin@gmail.com>
2023-06-07 12:52:41 +0000

    * Added rocketmq-all-5.1.1-bin-release

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+DIR=/opt
+echo "Installing..."
+cp -aR rootfs/* /opt
+echo "Finished"
---
+++ rootfs/opt/rocketmq-all-5.1.1-bin-release/rocketmq-brockera.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+cd /opt/rocketmq/bin
+start_brokera() {
+./mqnamesrv &
+sleep 5
+./mqbroker -n rocketmq-nameserver1:9876 -c ../conf/2m-2s-sync/broker-a.properties --enable-proxy &
+./mqbroker -c ../conf/2m-2s-sync/broker-b-s.properties &
+}
+
+start_brokerb() {
+./mqnamesrv &
+sleep 5
+./mqbroker rocketmq-nameserver2:9876 -c ../conf/2m-2s-sync/broker-b.properties --enable-proxy &
+./mqbroker -c ../conf/2m-2s-sync/broker-a-s.properties &
+}
+
+start() {
+ start_brokera
+}
+
+stop() {
+./mqshutdown broker
+./mqshutdown namesrv
+./mqshutdown controller
+./mqshutdown proxy
+}
+
+case "$1" in
+ start)
+   start
+ ;;
+ stop)
+   stop
+ ;;
+ restart)
+   stop
+   sleep 1
+   start
+ ;;
+ *)
+   echo "Usage $0 {start|stop|restart}"
+   exit 1
+ esac
+
+exit 0
+
